41fc83e275068555c09ab72204beb45b60e0aab9
ago yang <yangago668@gmail.com>
2022-01-31 12:10:02 +0800

    play test

--- read_midi.py
+++ read_midi.py
@@ -5,4 +5,9 @@ import time
 
 data_midi = MidiFile("/Users/liuxinhong/Documents/毕业论文/数据集/Summer.midi")
 
-print(mido.get_output_names())
+print(mido.get_output_names())  #print all available output ports
+
+port = mido.open_output('Virtual Test Bus 1')
+
+for msg in data_midi.play():
+    port.send(msg)
